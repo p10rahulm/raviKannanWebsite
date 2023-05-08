b0d6f4e060b1dc0b3b96ef56e8bead5cf2e1f18e
Rahul Madhavan <rahulmaddy@github>
2023-05-09 03:03:45 +0530

    content changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,3 +10,29 @@ function clickTalk(link_name) {
     console.log('myLink='+myLink)
     window.location = myLink;
 }
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyHeader()};
+
+
+function stickyHeader(){
+    if (headerTitle==null || stickyOffset==null || headerElem==null || logoImage==null){
+        var headerElem = document.getElementsByClassName("header")[0]
+        var logoImage = document.getElementById('HeaderLogo')
+        var headerTitle = document.getElementById('HeaderTitle')
+        var stickyOffset = headerTitle.offsetTop;
+        // console.log("stickyOffset=",stickyOffset)
+    }
+    if (window.innerWidth <= 515) {
+        if (window.pageYOffset > stickyOffset) {
+            headerElem.classList.add("sticky");
+            logoImage.classList.add("disappear")
+        } else {
+            headerElem.classList.remove("sticky");
+            logoImage.classList.remove("disappear")
+        }
+    } else {
+        headerElem.classList.add("sticky");
+    }
+};
